e7d663a83df42ee7d4c2d3ca856dafbbd9fb5b79
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-10-31 22:43:34 +1100

    refactor(sql): new table structure

--- ox_property.sql
+++ ox_property.sql
@@ -1,8 +1,5 @@
 CREATE TABLE IF NOT EXISTS `ox_property` (
-  `property` VARCHAR(50) NOT NULL,
-  `type` VARCHAR(50) NOT NULL,
-  `id` TINYINT(4) NOT NULL,
-  `owner` VARCHAR(50) NOT NULL,
-  `groups` TEXT NOT NULL DEFAULT '{}',
-  `public` TINYINT(1) NOT NULL DEFAULT 0
+  `name` VARCHAR(50) NOT NULL,
+  `owner` INT(11),
+  `permissions` TEXT NOT NULL DEFAULT '[]'
 );
