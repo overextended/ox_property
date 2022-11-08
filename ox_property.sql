63ab081434fbf5c27af53d4cc2ed75196b681bea
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-11-08 14:44:23 +1100

    fix(sql): default permissions value

--- ox_property.sql
+++ ox_property.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `ox_property` (
   `name` VARCHAR(50) NOT NULL,
   `owner` INT(11),
   `group` VARCHAR(50),
-  `permissions` TEXT NOT NULL DEFAULT '[]'
+  `permissions` TEXT NOT NULL DEFAULT '[[]]'
 );
