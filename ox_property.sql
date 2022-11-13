2513b6266781c2317f5dd87536f9ae33df9fd915
Linden <65407488+thelindat@users.noreply.github.com>
2022-11-13 15:18:20 +1100

    refactor(sql): set owner foreign key constraint

--- ox_property.sql
+++ ox_property.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `ox_property` (
   `name` VARCHAR(50) NOT NULL,
-  `owner` INT(11),
+  `owner` INT(11) UNSIGNED,
   `group` VARCHAR(50),
-  `permissions` TEXT NOT NULL DEFAULT '[{groups:{}}]'
+  `permissions` TEXT NOT NULL DEFAULT '[{groups:{}}]',
+  INDEX `FK_ox_property_characters` (`owner`) USING BTREE,
+  CONSTRAINT `FK_ox_property_characters` FOREIGN KEY (`owner`) REFERENCES `characters` (`charid`) ON UPDATE CASCADE ON DELETE SET NULL
 );
