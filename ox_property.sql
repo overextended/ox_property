33da90ab0f8b419fc037102e941f49e6b4c21add
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-11-13 17:49:24 +1100

    Revert "tweak: include groups table in default permissions"
    This reverts commit 774224a2493e7270fb3153ac7a9aaebd329dde86.

--- ox_property.sql
+++ ox_property.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `ox_property` (
   `name` VARCHAR(50) NOT NULL,
   `owner` INT(11) UNSIGNED,
   `group` VARCHAR(50),
-  `permissions` TEXT NOT NULL DEFAULT '[{groups:{}}]',
+  `permissions` TEXT NOT NULL DEFAULT '[{}]',
   INDEX `FK_ox_property_characters` (`owner`) USING BTREE,
   CONSTRAINT `FK_ox_property_characters` FOREIGN KEY (`owner`) REFERENCES `characters` (`charid`) ON UPDATE CASCADE ON DELETE SET NULL
 );
