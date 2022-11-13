c65c5721ba384343ad5a0489106cc53c60ac1e2b
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-11-13 13:24:39 +1100

    tweak: include groups table in default permissions

--- server/main.lua
+++ server/main.lua
@@ -82,7 +82,7 @@ local function loadResourceDataFiles()
         else
             v.owner = defaultOwner
             v.ownerName = defaultOwnerName
-            v.permissions = {{}}
+            v.permissions = {{groups = {}}}
             v.group = defaultGroup
             v.groupName = defaultGroupName
 
