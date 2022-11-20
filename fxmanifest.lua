e663bd9881a76134593b1ece5140b3ac8a623ba8
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-11-20 20:11:49 +1100

    chore: version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game         'gta5'
 
 --[[ Resource Information ]]--
 name         'ox_property'
-version      '0.7.1'
+version      '0.8.0'
 description  'Property'
 license      'GPL-3.0-or-later'
 author       'overextended'
